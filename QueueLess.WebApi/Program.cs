0e52ea2004eff5cb06c73a763204955f25971632
Ruther.f <robelfikadu70@gmail.com>
2026-08-17 22:06:15 +0300

    feat: implement jwt authentication services and token configuration

--- QueueLess.WebApi/Program.cs
+++ QueueLess.WebApi/Program.cs
@@ -1,4 +1,10 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using QueueLess.Application.Interfaces;
+using QueueLess.Infrastructure.Identity;
 using QueueLess.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,9 +16,45 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 //2 Register the DbContext as Scoped (default) using PostgreSQL
 builder.Services.AddDbContext<QlDbContext>(options => options.UseNpgsql(connectionString, b => b.MigrationsAssembly("QueueLess.Infrastructure"))); //Generates migration inside Infrastructure
 
-// Add services to the container.
+//3. Identity Service Setup
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequiredLength = 6;
+})
+.AddEntityFrameworkStores<QlDbContext>()
+.AddDefaultTokenProviders();
+
+// 4. Jwt Authentication configuration
+var jwtSecret = builder.Configuration["JwtSettings:Secret"]
+    ?? throw new InvalidOperationException("JWT Secret is not configured.");
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters =  new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+        ValidAudience = builder.Configuration["JwtSettings:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecret))
+    };
+});
+
+// 5. Interface Registration (DI)
+builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
@@ -24,7 +66,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseAuthentication(); //Determines WHO is requesting
+app.UseAuthorization(); //Determines WHAT they can access
 
 app.MapControllers();
 
